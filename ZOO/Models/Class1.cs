4c1c7b4f5104c2842e487ac1e6d22c06f7f5288b
filip.matuszczak <filip.matuszczak@student.put.poznan.pl>
2019-01-04 22:47:29 +0100

    Wow now we have mongo :)))
    Initialized mongo and checking if it works

---
+++ ZOO/Models/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ZOO.Models
+{
+    public class Class1
+    {
+        public class FeedingReminder
+        {
+            public ObjectId Id { get; set; }
+            [BsonElement("FeedingId")]
+            public int FeedingId { get; set; }
+            [BsonElement("FeedingDate")]
+            public string FeedingDate { get; set; }
+            [BsonElement("WasShown")]
+            public bool WasShown { get; set; }
+        }
+    }
+}
