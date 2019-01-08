6bb08e0e307172680fbbb2289e9af847c60682e6
filip.matuszczak <filip.matuszczak@student.put.poznan.pl>
2019-01-09 00:39:44 +0100

    Mongo rescued

---
+++ ZOO/Controllers/FeedingReminderController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZOO.Models;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace ZOO.Controllers
+{
+    public class FeedingReminderController : Controller
+    {
+        private ZOOEntities db = new ZOOEntities();
+
+        public ActionResult Index()
+        {
+
+            FeedingReminderAccess feedingReminderAccess = new FeedingReminderAccess();
+
+            IEnumerable<FeedingReminder> allReminders = feedingReminderAccess.GetFeedingReminders();
+            FeedingReminder[] reminders = allReminders.Cast<FeedingReminder>().ToArray();
+
+            List<int> ids = filterIds(reminders);
+
+            var feedings = db.Feedings.Include(f => f.AnimalGroups).Include(f => f.Employees).Include(f => f.FoodProducts).Where(f=>ids.Contains(f.FeedingId));
+            return View(feedings.ToList());
+        }
+
+        public ActionResult MarkAsShown(int? id)
+        {
+            FeedingReminderAccess feedingReminderAccess = new FeedingReminderAccess();
+
+            IEnumerable<FeedingReminder> allReminders = feedingReminderAccess.GetFeedingReminders();
+            FeedingReminder[] reminders = allReminders.Cast<FeedingReminder>().ToArray();
+
+            for (int i = 0; i < reminders.Length; i++)
+            {
+                if (reminders[i].FeedingId == id)
+                {
+                    reminders[i].WasShown = 1;
+                    feedingReminderAccess.Update(reminders[i].Id, reminders[i]);
+                }
+
+            }
+
+            List<int> ids = filterIds(reminders);
+
+            var feedings = db.Feedings.Include(f => f.AnimalGroups).Include(f => f.Employees).Include(f => f.FoodProducts).Where(f => ids.Contains(f.FeedingId));
+
+            return View(feedings.ToList());
+        }
+
+        private List<int> filterIds(FeedingReminder[] reminders) {
+            List<int> ids = new List<int>();
+
+            for (int i = 0; i < reminders.Length; i++)
+            {
+                DateTime myDate = DateTime.Parse(reminders[i].FeedingDate);
+                if (myDate == DateTime.Today && reminders[i].WasShown == 0)
+                {
+                    ids.Add(reminders[i].FeedingId);
+                }
+
+            }
+            return ids;
+        }
+    }
+
+   
+
+    
+}
---
+++ ZOO/Models/FeedingReminder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ZOO.Models
+{
+   
+        public class FeedingReminder
+        {
+            public ObjectId Id { get; set; }
+            [BsonElement("FeedingId")]
+            public int FeedingId { get; set; }
+            [BsonElement("FeedingDate")]
+            public string FeedingDate { get; set; }
+            [BsonElement("WasShown")]
+            public int WasShown { get; set; }
+
+        public FeedingReminder(int feedingId, string feedingDate, int wasShown) {
+            FeedingId = feedingId;
+            FeedingDate = feedingDate;
+            WasShown = wasShown;
+        }
+        }
+    }
